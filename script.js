170ee0f9cd29713d8c9d2730e37416047c30a7ae
ngoc <ngo.khanh.ngoc@gmail.com>
2019-11-06 20:57:38 -0800

    Revise JS outline

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ NOT NEEDED 3. Function to parse API data
   - Input: unparsed API data, array of desired fields
   - If desired fields empty, return all
   - Return: parsed JSON data, limited only to desired fields
-N4. Function Apply filter to last search results
+*N4. Function Apply filter to last search results
   - Return: new parsed JSON data object
-N5. Function to render HTML cards
+*N5. Function to render HTML cards
   - Input: parsed JSON data, limited only to desired fields
   - Return: Array of template string populated with data
 *D6. Function to convert border country names
@@ -20,16 +20,12 @@ N5. Function to render HTML cards
 *D7. Function to populate cards section with rendered cards
   - Input: array of template strings populated with data
 
-A. Control function
-  - Input: search string, type of query (new search, or apply filter)
-  - If new search:
-    - Fetch and parse
-  - If both applying new filter or a new search:
-    - Clear display
-    - Apply filter to last search result
-    - Render HTML and populate
+*A. Search function
+  - Input: search string
+  - Fetch and parse
+  - Filter
 
-B. Onload
+*B. Onload
   - Calls fetch on all countries
   1. Function to create object of country codes
     - Input: Unparsed data of all countries
@@ -41,7 +37,7 @@ B. Onload
     - Calls function to render HTML cards
     - Calls function to populate cards
 
-C. Function to update global filter settings (on filter click)
+*C. Function to update global filter settings (on filter click)
   - Identify which filter was clicked
   - Toggle global setting for that filter
   - Call control function to rerender section
@@ -63,6 +59,7 @@ D. Back button handler
     it will only have detail-on toggled off and other small hidden cards display restored
     to initial.
 
+E. Detail view
 
 */
 
